446dc1275c3ebde5222a0f6fceca4df449ec4e97
minu0210 <113428736+minu0210@users.noreply.github.com>
2023-06-24 23:12:20 +0900

    [Bronze V] Title: 사칙연산, Time: 64 ms, Memory: 5500 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cs
@@ -0,0 +1,7 @@
+string[] s = Console.ReadLine().Split();
+
+Console.WriteLine(int.Parse(s[0]) + int.Parse(s[1]));
+Console.WriteLine(int.Parse(s[0]) - int.Parse(s[1]));
+Console.WriteLine(int.Parse(s[0]) * int.Parse(s[1]));
+Console.WriteLine(int.Parse(s[0]) / int.Parse(s[1]));
+Console.WriteLine(double.Parse(s[0]) % double.Parse(s[1]));
