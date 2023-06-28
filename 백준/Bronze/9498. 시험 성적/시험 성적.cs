488db7a56ca231469f74f22707629c309fc31923
minu0210 <113428736+minu0210@users.noreply.github.com>
2023-06-28 09:31:18 +0900

    [Bronze V] Title: 시험 성적, Time: 76 ms, Memory: 5464 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.cs
@@ -0,0 +1,7 @@
+int num = int.Parse(Console.ReadLine());
+
+if (num >= 90 && num <= 100) Console.WriteLine("A");
+else if (num >= 80) Console.WriteLine("B");
+else if (num >= 70) Console.WriteLine("C");
+else if (num >= 60) Console.WriteLine("D");
+else Console.WriteLine("F");
